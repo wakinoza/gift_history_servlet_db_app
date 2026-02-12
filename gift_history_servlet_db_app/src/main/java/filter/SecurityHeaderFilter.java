22403d9649b318694abf7197f761d97d01f3837e
wakinoza <wakinoza@gmail.com>
2026-02-12 15:56:45 +0900

    セキュリティ対策のヘッダを指定するfilterクラスを追加

---
+++ gift_history_servlet_db_app/src/main/java/filter/SecurityHeaderFilter.java
@@ -0,0 +1,30 @@
+package filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class SecurityHeaderFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+
+    HttpServletResponse res = (HttpServletResponse) response;
+
+    res.setContentType("text/html; charset=UTF-8");
+    res.setHeader("Content-Security-Policy",
+        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';");
+    res.setHeader("X-Frame-Options", "DENY");
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader("X-XSS-Protection", "1; mode=block");
+
+    chain.doFilter(request, response);
+  }
+}
