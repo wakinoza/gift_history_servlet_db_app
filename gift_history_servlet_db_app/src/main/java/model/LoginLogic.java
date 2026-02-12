f5969cba2a81cd2aa8b280bbd88f593ddf3dbf51
wakinoza <wakinoza@gmail.com>
2026-02-12 15:12:00 +0900

    ログイン入力値のバリデーションメソッドを追記

--- gift_history_servlet_db_app/src/main/java/model/LoginLogic.java
+++ gift_history_servlet_db_app/src/main/java/model/LoginLogic.java
@@ -6,6 +6,8 @@ import dao.UserDAO;
  * ログイン時の認証を行うクラス.
  */
 public class LoginLogic {
+  final int MAX_NAME_LENGTH = 50;
+  final int MAX_PASS_LENGTH = 100;
 
   /**
    * ログイン時の認証を行うメソッド.
@@ -15,8 +17,32 @@ public class LoginLogic {
    * @return 認証されたUserインスタンス
    */
   public User execute(String name, String pass) {
+
+
+    if (!isValidInput(name, pass)) {
+      return null;
+    }
     UserDAO userDao = new UserDAO();
     return userDao.select(name, pass);
+  }
+
+  /**
+   * 入力値のバリデーションを行うメソッド.
+   * 
+   * @param name ユーザー名
+   * @param pass パスワード
+   * @return バリデーション結果
+   */
+  private boolean isValidInput(String name, String pass) {
+    if (name == null || name.isBlank() || pass == null || pass.isBlank()) {
+      return false;
+    }
+
+    if (name.length() > MAX_NAME_LENGTH || pass.length() > MAX_PASS_LENGTH) {
+      return false;
+    }
+
+    return true;
   }
 }
 
