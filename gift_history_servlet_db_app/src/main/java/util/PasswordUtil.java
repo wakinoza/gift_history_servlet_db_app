ebe0dd6d23ae46145df567cd91dae97614b1f94b
wakinoza <wakinoza@gmail.com>
2026-02-13 14:07:55 +0900

    パスワードハッシュ化用のユーティリティクラスを追加

---
+++ gift_history_servlet_db_app/src/main/java/util/PasswordUtil.java
@@ -0,0 +1,42 @@
+package util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * パスワードをハッシュ化するユーティリティクラス.
+ */
+public class PasswordUtil {
+  private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+  /**
+   * パスワードをハッシュ化するメソッド.
+   * 
+   * @param rawPassword ユーザー入力のパスワード
+   * @return ハッシュ値
+   */
+  public static String hash(String rawPassword) {
+    return encoder.encode(rawPassword);
+  }
+
+  /**
+   * 入力したパスワードと、DBのハッシュ値とを比較するメソッド.
+   * 
+   * @param rawPassword ユーザー入力のパスワード
+   * @param hashed DBに格納されたハッシュ値
+   * @return 照合結果
+   */
+  public static boolean check(String rawPassword, String hashed) {
+    return encoder.matches(rawPassword, hashed);
+  }
+
+  /**
+   * ハッシュ値を出力するメソッド.
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    String myPass = "default_user_password";
+    System.out.println("生成されたハッシュ値: " + hash(myPass));
+  }
+
+}
