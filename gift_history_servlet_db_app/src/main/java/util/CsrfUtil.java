f7970f190a9bc1fb17521847980c5e8eec43ca57
wakinoza <wakinoza@gmail.com>
2026-03-16 14:20:04 +0900

    Utilクラスであるため、プライベートコンストラクタを追加

--- gift_history_servlet_db_app/src/main/java/util/CsrfUtil.java
+++ gift_history_servlet_db_app/src/main/java/util/CsrfUtil.java
@@ -4,6 +4,14 @@ import java.security.SecureRandom;
 import java.util.Base64;
 
 public class CsrfUtil {
+
+  /**
+   * プライベートコンストラクタでインスタンス化を禁止する
+   */
+  private CsrfUtil() {
+    throw new AssertionError("Instantiating utility class.");
+  }
+
   /**
    * CSRF対策用のランダムなトークンを生成する
    */
