46b35e4fadb2d172cecfd21dcd57791df1c2e00e
wakinoza <wakinoza@gmail.com>
2026-06-10 16:51:44 +0900

    ViewGiftDetailサーブレットの内容を、GIｆｔItemControllerに移行する
    と不要になったクラスの削除

--- gift_history_servlet_db_app/src/main/java/com/gift/app/controller/GiftItemController.java
+++ gift_history_servlet_db_app/src/main/java/com/gift/app/controller/GiftItemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,6 +67,44 @@ public class GiftItemController {
       newItem.setNeedReturn(needReturn);
 
     giftItemRepository.save(newItem);
+
+    return "redirect:/gift/main";
+  }
+
+  /**
+   * 頂き物詳細画面の表示
+   */
+  @GetMapping("/detail/{id}")
+  public String showDetailPage(@PathVariable("id") int id, Model model) {
+    GiftItem currentGiftItem = giftItemRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid gift Item Id:" + id));
+
+    model.addAttribute("currentGiftItem", currentGiftItem);
+    return "gift/detail";
+
+  }
+
+  /**
+   * 返礼済みステータスの更新処理
+   */
+  @PostMapping("/detail/{id}/return")
+  public String updateToReturned(@PathVariable("id") int id) {
+    GiftItem giftItem = giftItemRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid gift Item Id:" + id));
+
+    giftItem.setHasGaveReturn("済");
+
+    giftItemRepository.save(giftItem);
+
+    return "redirect:/gift/main";
+  }
+
+  /**
+   * 頂き物の削除処理
+   */
+  @PostMapping("/detail/{id}/delete")
+  public String deleteGift(@PathVariable("id") int id) {
+    giftItemRepository.deleteById(id);
 
     return "redirect:/gift/main";
   }
