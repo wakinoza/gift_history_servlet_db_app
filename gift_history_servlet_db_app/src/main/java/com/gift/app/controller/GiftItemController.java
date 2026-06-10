f5c8a0700ae8f6961b077e2fa5c744451f47a5d0
wakinoza <wakinoza@gmail.com>
2026-06-10 16:46:27 +0900

    GiftItemControllerにNewGifサーブレットの処理を追加
    と、不要になったクラス削除

--- gift_history_servlet_db_app/src/main/java/com/gift/app/controller/GiftItemController.java
+++ gift_history_servlet_db_app/src/main/java/com/gift/app/controller/GiftItemController.java
@@ -1,10 +1,13 @@
 package com.gift.app.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.gift.app.entity.GiftItem;
 import com.gift.app.repository.GiftItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -30,4 +33,40 @@ public class GiftItemController {
   }
 
 
+  /**
+   * 頂き物新規登録画面の表示
+   */
+  @GetMapping("/new")
+  public String showNewGiftPage() {
+    return "gift/new";
+  }
+
+  /**
+   * 頂き物新規登録処理の実行
+   */
+  @PostMapping("/new")
+  public String createNewGift(@RequestParam("what") String what,
+      @RequestParam("whenis") LocalDate whenis, @RequestParam("who") String who,
+      @RequestParam("why") String why, @RequestParam("howMuch") String howMuch,
+      @RequestParam("needReturn") String needReturn) {
+
+    GiftItem newItem = new GiftItem();
+
+    if (what != null && !what.isBlank())
+      newItem.setWhat(what);
+    if (whenis != null)
+      newItem.setWhenis(whenis);
+    if (who != null && !who.isBlank())
+      newItem.setWho(who);
+    if (why != null && !why.isBlank())
+      newItem.setWhy(why);
+    if (howMuch != null && !howMuch.isBlank())
+      newItem.setHowMuch(howMuch);
+    if (needReturn != null && !needReturn.isBlank())
+      newItem.setNeedReturn(needReturn);
+
+    giftItemRepository.save(newItem);
+
+    return "redirect:/gift/main";
+  }
 }
