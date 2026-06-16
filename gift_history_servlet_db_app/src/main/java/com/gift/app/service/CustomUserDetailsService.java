1f178c9b1fc578fbdf29c49be10848a31d2973e6
wakinoza <wakinoza@gmail.com>
2026-06-16 16:52:58 +0900

    カスタムUserクラスを認知するように、完全修飾クラス名記載した。

--- gift_history_servlet_db_app/src/main/java/com/gift/app/service/CustomUserDetailsService.java
+++ gift_history_servlet_db_app/src/main/java/com/gift/app/service/CustomUserDetailsService.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import com.gift.app.entity.User;
 import com.gift.app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -16,10 +15,10 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userRepository.findByName(username)
+    com.gift.app.entity.User dbUser = userRepository.findByName(username)
         .orElseThrow(() -> new UsernameNotFoundException("ユーザーが見つかりません: " + username));
 
-    return org.springframework.security.core.userdetails.User.withUsername(user.getName())
-        .password(user.getPassword()).roles("USER").build();
+    return org.springframework.security.core.userdetails.User.withUsername(dbUser.getName())
+        .password(dbUser.getPassword()).roles("USER").build();
   }
 }
