5f831e5aabf01b85e5fd798711a0b65b511a0ffb
wakinoza <wakinoza@gmail.com>
2026-06-18 15:37:06 +0900

    ログの実装

--- gift_history_servlet_db_app/src/main/java/com/gift/app/controller/LoginController.java
+++ gift_history_servlet_db_app/src/main/java/com/gift/app/controller/LoginController.java
@@ -3,8 +3,9 @@ package com.gift.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 @Controller
 public class LoginController {
 
@@ -13,6 +14,7 @@ public class LoginController {
    */
   @GetMapping("/login")
   public String showLoginPage() {
+    log.info("ログイン画面がリクエストされました。");
     return "login";
   }
 }
