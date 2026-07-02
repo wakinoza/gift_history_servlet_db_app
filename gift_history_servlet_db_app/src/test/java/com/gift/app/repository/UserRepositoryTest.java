2dc80d7a8bbf353e5682d44ac50defef57405363
wakinoza <wakinoza@gmail.com>
2026-07-02 17:07:32 +0900

    @Transactionalを記述し忘れていたため、記述する

--- gift_history_servlet_db_app/src/test/java/com/gift/app/repository/UserRepositoryTest.java
+++ gift_history_servlet_db_app/src/test/java/com/gift/app/repository/UserRepositoryTest.java
@@ -5,14 +5,15 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import com.gift.app.entity.User;
 
-@DataJpaTest
-@ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
+@SpringBootTest
+// @DataJpaTest
+// @ActiveProfiles("test")
+// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
   @Autowired
--- gift_history_servlet_db_app/src/test/java/com/gift/app/service/CustomUserDetailsServiceTest.java
+++ gift_history_servlet_db_app/src/test/java/com/gift/app/service/CustomUserDetailsServiceTest.java
@@ -11,9 +11,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 import com.gift.app.entity.User;
 import com.gift.app.repository.UserRepository;
 
+@Transactional
 @ExtendWith(MockitoExtension.class)
 class CustomUserDetailsServiceTest {
 
