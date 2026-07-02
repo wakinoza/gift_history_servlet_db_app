e7b75eaa36624a420a03e552eac6c4675e4ca932
wakinoza <wakinoza@gmail.com>
2026-07-02 14:54:57 +0900

    Docker環境でテストができるようにアノテーションを追加

--- gift_history_servlet_db_app/src/test/java/com/gift/app/repository/GiftItemRepositoryTest.java
+++ gift_history_servlet_db_app/src/test/java/com/gift/app/repository/GiftItemRepositoryTest.java
@@ -8,9 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import com.gift.app.entity.GiftItem;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class GiftItemRepositoryTest {
 
